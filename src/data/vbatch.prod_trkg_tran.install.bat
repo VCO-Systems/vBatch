98442ad17d967c4f690721bcbefdbaa873102705
nuevia <vcahall@gmail.com>
2014-03-05 10:55:17 -0500

    cleaning up install scripts

--- src/data/vbatch.prod_trkg_tran.install.bat
+++ src/data/vbatch.prod_trkg_tran.install.bat
@@ -1,3 +1,3 @@
 sqlplus vbatch/vbatch @ddl/vbatch.prod_trkg_tran.ddl
 PAUSE
-sqlplus vbatch/vbatch @sample_data/prod_trkg_tran__sample_data_1.sql
+sqlplus vbatch/vbatch @sample_data/prod_trkg_tran-ohl-export-after-07-Feb-14-12.58.00.sql
