463744bd50b4553565a726a8359cb892a749fe08
vco-van <van.cahall@vcosystems.com>
2014-03-19 20:05:37 -0400

    added GenerateCSVStep.quoteAllOutputFields property (defaults to false) which satisfies VBAT-37

--- src/com/vco/GenerateCSVStep.java
+++ src/com/vco/GenerateCSVStep.java
@@ -26,6 +26,9 @@ import model.BatchLogFileOutput;
 import model.Step;
 
 public class GenerateCSVStep extends StepManager {
+	
+	// Step configuration
+	public Boolean quoteAllOutputFields = false;
 
 	/**
 	 * This step uses the optional mode_special flag
@@ -320,11 +323,13 @@ public class GenerateCSVStep extends StepManager {
 		String retval = "";
 		for (int i=0; i < row.size(); i++) {
 			String fieldVal = (String) row.get(i);
-			retval += "\"";
+			if (this.quoteAllOutputFields)
+				retval += "\"";
 			if (row.get(i) != null) {
 				retval += row.get(i);
 			}
-			retval += "\"";
+			if (this.quoteAllOutputFields)
+				retval += "\"";
 			// Add comma after field (unless last field)
 			String comma = (i < row.size() - 1) ? "," : "";
 			retval += comma;
